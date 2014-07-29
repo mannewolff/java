a685c9ff959020158baedb55fd34a8d64cbf4570
mwolff <wolff@manfred-wolff.de>
2014-07-29 12:52:33 +0200

    Initial login

---
+++ command-example/src/test/java/de/neusta/login/validator/TestLoginValidators.java
@@ -0,0 +1,39 @@
+package de.neusta.login.validator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestLoginValidators {
+
+	@Test
+	public void testLoginContext() throws Exception {
+
+		LoginContext loginContext = new LoginContext();
+		loginContext.setName("mwolff");
+		assertEquals("mwolff", loginContext.getName());
+
+		loginContext.setOriginalPasswd("geheim");
+		assertEquals("d5819db235972a3998f7c290ae583664",
+				loginContext.getPasswd());
+
+	}
+
+	@Test(expected=Exception.class)
+	public void testLengthValidator() throws Exception {
+
+		LoginContext context = new LoginContext();
+		LengthValidator<LoginContext> lengthValidator = new LengthValidator<LoginContext>();
+		lengthValidator.setLength(14);
+		context.setOriginalPasswd("12Characters");
+		lengthValidator.execute(context);
+		assertEquals("The password has to be as minimum 14 characters",
+				context.getErrorMessage());
+
+	}
+	
+	@Test
+	public void testLengthValidatorAsChain() throws Exception {
+		
+	}
+}

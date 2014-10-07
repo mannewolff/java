5eaf94ef54eee63c2548cbc84762abba3a8993ed
Manfred Wolff <m.wolff@neusta.de>
2014-10-07 09:00:14 +0200

    Ausgangsposition

--- command-example/src/test/java/de/neusta/ugly/TestUglyImplementation.java
+++ command-example/src/test/java/de/neusta/ugly/TestUglyImplementation.java
@@ -6,15 +6,5 @@ import org.junit.Test;
 
 public class TestUglyImplementation {
 
-	@Test
-	public void testLengthMethod() throws Exception {
-		
-		LoginValidator loginValidator = new LoginValidator("geheim");
-		boolean result = loginValidator.validateLength();
-		assertFalse(result);
-		if (! result) {
-			String error = loginValidator.getErrorMessage();
-			assertEquals("The password has to have at minimum 12 characters", error);
-		}
-	}
+	
 }

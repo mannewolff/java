564456d6d1c94bb83459f67e7af0b47c84814856
Manfred Wolff <wolff@manfred-wolff.de>
2015-02-12 17:28:38 +0100

    add ConfigController

---
+++ SpringMVC/src/main/java/de/neusta/common/controller/ConfigController.java
@@ -0,0 +1,113 @@
+package de.neusta.common.controller;
+
+import static de.neusta.common.controller.ControllerConstants.CONFIG_INPUT_PAGE;
+import static de.neusta.common.controller.ControllerConstants.CONFIG_LIST;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import de.neusta.persistence.entity.User;
+
+@Controller
+public class ConfigController extends AspectController {
+
+	static Logger log = Logger.getLogger(ConfigController.class);
+
+	/*
+	@Resource
+	ConfigService userService;
+
+	@Resource
+	ConfigFacade userFacade;
+	*/
+
+	/**
+	 * Prepares the data for creating and updating a user.
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/preconfig")
+	protected ModelAndView prepareConfigDataInput(@RequestParam Long id)
+			throws Exception {
+
+		// logging
+		beginMethod(log, "prepareUserDataInput @RequestMapping=/preuser.");
+
+		// performing
+		final ModelAndView model = new ModelAndView(CONFIG_INPUT_PAGE);
+		//User user = userFacade.createUser(id);
+		//model.addObject("User", user);
+
+		// logging
+		endMethod();
+
+		return model;
+	}
+
+	@RequestMapping("/addconfig")
+	@Transactional
+	protected ModelAndView addConfig(@ModelAttribute User user) throws Exception {
+
+		// logging
+		beginMethod(log, "addUser @RequestMapping=/adduser.");
+		log.debug("User ist " + user);
+
+		// performing
+		/*
+		if (user == null) {
+			log.error("User in scope is null.");
+			return prepareUserDataInput(0l);
+		}
+		
+		userFacade.saveOrUpdate(user);
+		*/
+
+		// logging
+		endMethod();
+
+		return listConfigs();
+	}
+
+	@RequestMapping("/listconfigs")
+	@Transactional
+	protected ModelAndView listConfigs() throws Exception {
+
+		// logging
+		beginMethod(log, "listUser @RequestMapping=/listuser.");
+
+		// performing
+		
+		final ModelAndView model = new ModelAndView(CONFIG_LIST);
+		/*
+		List<User> userlist = userFacade.getAllUser();
+		model.addObject("Userlist", userlist);
+		*/
+
+		// logging
+		endMethod();
+
+		return model;
+
+	}
+
+	@RequestMapping("/deleteconfig")
+	@Transactional
+	protected ModelAndView deleteUser(@RequestParam Long id) throws Exception {
+
+		// logging
+		beginMethod(log, "listUser @RequestMapping=/deleteuser.");
+
+		// performing
+
+		// logging
+		endMethod();
+
+		return listConfigs();
+	}
+}

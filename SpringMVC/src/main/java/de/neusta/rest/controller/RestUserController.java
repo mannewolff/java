07c904b96155eebe8ba67789016189f8806489fd
Manfred Wolff <wolff@manfred-wolff.de>
2015-03-20 14:43:11 +0100

    Zweiter Versuch Rest

---
+++ SpringMVC/src/main/java/de/neusta/rest/controller/RestUserController.java
@@ -0,0 +1,27 @@
+package de.neusta.rest.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RestUserController {
+
+	static Logger log = Logger.getLogger(RestUserController.class);
+	private static final String INVALID = "-1";
+
+	@RequestMapping(value="/rest/{uid}")
+	protected String prepareUserDataInput(@PathVariable("uid") final String uid) {
+
+		// performing
+		log.debug("REST --> ");
+		//log.debug("REST --> " + uid);
+		//log.debug("REST --> " + pwd);
+
+		// logging
+
+		return null;
+	}
+
+}

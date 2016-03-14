2cd5959c248c78a0fa6655bcf5283d778ffaf6cd
Manfred Wolff <m.wolff@neusta.de>
2016-03-14 14:31:06 +0100

    minor changes.

--- mockito/src/test/java/de/neusta/mock/basic/TestMockBasic.java
+++ mockito/src/test/java/de/neusta/mock/basic/TestMockBasic.java
@@ -1,7 +1,7 @@
 package de.neusta.mock.basic;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.Test;
